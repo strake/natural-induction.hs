a7e16469a2173c45952d59231eec983d83c4f44f
M Farkas-Dyck <strake888@gmail.com>
2017-12-13 21:55:12 -0800

    reify

--- Data/Natural/Class.hs
+++ Data/Natural/Class.hs
@@ -1,6 +1,8 @@
 module Data.Natural.Class where
 
+import Data.Functor.Const
 import Data.Peano
+import Data.Proxy
 
 class Natural n where
     natural :: f Zero -> (∀ m . Natural m => f (Succ m)) -> f n
@@ -10,3 +12,7 @@ instance Natural Zero where
 
 instance Natural n => Natural (Succ n) where
     natural _ sf = sf
+
+reify :: Natural n => Proxy n -> Peano
+reify (Proxy :: Proxy n) = getConst $ natural @n (Const Zero) (φ $ Const . Succ . reify)
+  where φ = ($ Proxy) :: ∀ f a . (Proxy a -> f a) -> f a
